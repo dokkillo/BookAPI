54ceb6cfb31da7e85e368389458220fc5193bd89
Gabriel C <dokkillo@users.noreply.github.com>
2017-10-01 20:45:39 +0200

    init

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
 
 namespace BookAPI
 {
@@ -10,6 +8,20 @@ namespace BookAPI
     {
         static void Main(string[] args)
         {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
         }
     }
 }
