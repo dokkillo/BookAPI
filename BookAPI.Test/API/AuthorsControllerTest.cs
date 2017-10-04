22ac56d63acf3ea432a2e80080f2de5095b5c1b1
Gabriel C <dokkillo@users.noreply.github.com>
2017-10-04 07:15:25 +0200

    Test projects

---
+++ BookAPI.Test/API/AuthorsControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookAPI.Controller;
+using System.Web.Http.Results;
+using System.Linq;
+
+
+namespace BookAPI.Test.API
+{
+    [TestClass]
+    public class AuthorsControllersTest
+    {
+
+        public AuthorsControllersTest()
+        {
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<BookAPI.DAL.Entities.Author, BookAPI.Model.AuthorDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"));
+
+            });
+
+        }
+
+
+        [TestMethod]
+        public void AuthorsConllersReturnValues()
+        {
+            var controller = new AuthorsController();
+            var result = controller.Get();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkResult));    
+
+        }
+    }
+}
