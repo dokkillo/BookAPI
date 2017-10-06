ec3a6d78c2e3ea52cf8ff45eb5db719c0d293ee0
Gabriel C <dokkillo@users.noreply.github.com>
2017-10-06 07:13:01 +0200

    Class SystemTime
    Added this class for manage time in project methods

---
+++ BookAPI.Utils/SystemTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace BookApi.Utils
+{
+    public static class SystemTime
+    {
+
+        #region Dates
+
+        /// <summary>
+        /// Fecha Actual
+        /// </summary>
+        public static Func<DateTime> Now = () => DateTime.Now;
+        
+        #endregion
+
+        #region Day Methods
+
+        /// <summary>
+        /// Dia actual
+        /// </summary>
+        public static Func<int> Day = () => DateTime.Now.Day;
+        
+
+        #endregion
+
+        #region Months Methods
+
+        /// <summary>
+        /// Mes actual
+        /// </summary>
+        public static Func<int> Month = () => DateTime.Now.Month;
+       
+
+        #endregion
+
+        #region Year Methods
+
+        /// <summary>
+        /// Año actual
+        /// </summary>
+        public static Func<int> Year = () => DateTime.Now.Year;
+
+       
+        #endregion
+
+
+    }
+
+}
